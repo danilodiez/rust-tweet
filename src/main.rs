dd9902b0c2ccc9cd445634e303afed9a1160b17e
Danilo Diez <daniloo10.dd@gmail.com>
2022-07-05 18:56:28 -0500

    more control structures

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,21 @@ let (x, y) = (4, 5);
 let mut z: i8 = 2;
 
 z = 3;
+// we can use _ to divide long numbers like
+let k: i16 = 100_000_000;
 
 println!("{}",adds_three(5));
+
+//yay f#cking ternary
+// let is_below_eigthteen = if age > 18 {true} else {false};
+
+// we also have case kinda with "match"
+
 }
+
+
 // We set the type of a variable with ":"
 fn adds_three(a: i8) -> i8 {
     return a + 3;
 }
+
