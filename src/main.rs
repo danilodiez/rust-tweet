75c3e1c5755b86b2ec76997355e69e00dc848df2
Danilo Diez <daniloo10.dd@gmail.com>
2022-07-20 17:04:06 -0500

    playing around

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ let mut z: i8 = 2;
 
 z = 3;
 // we can use _ to divide long numbers like
-let k: i16 = 100_000_000;
+//let k: i16 = 100_000_000;
 
 println!("{}",adds_three(5));
 
@@ -23,6 +23,24 @@ println!("{}",adds_three(5));
 
 // we also have case kinda with "match"
 
+//Trying strucs, impls and traits
+struct Player {
+    first_name: String,
+    last_name: String,
+}
+impl Player {
+    fn get_name(&self) -> String {
+       return format!("{} {}", self.first_name, self.last_name);           
+    }
+}
+let player_1 = Player {
+    first_name: "Danilo".to_string(),
+    last_name: "Diez".to_string(),
+};
+
+println!("Player 01: {}", player_1.get_name());
+let vector: Vec<i32> = vec![12, 32, 33, 43];
+map_vector(vector);
 }
 
 
@@ -30,4 +48,11 @@ println!("{}",adds_three(5));
 fn adds_three(a: i8) -> i8 {
     return a + 3;
 }
+
+fn map_vector(v: Vec<i32>){
+    for e in v {
+        println!("{}", e)
+    }
+}
+
 
